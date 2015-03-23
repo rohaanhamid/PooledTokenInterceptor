6956ba9a70c4e4ed60620cd9bb9c5b8ff77b1676
Rohaan Hamid <rohaanhamid@hotmail.com>
2015-03-23 06:08:57 -0400

    Backing up some ideas, still not complete

--- test.js
+++ test.js
@@ -1,4 +1,18 @@
 var R = require("./tokenpoolrequest"); 
+var Q = require("q");
+
+//1. Check limits on all token pools using given recipe
+//2. When token pool is exhausted, request new resets and wait for the earliest reset
+// if 'WTC' is selected. Else return all promise with an error of pool exhausted.
+//3. On wakeup, recursively call the function and make sure current request is not
+// executed. Once the request is successful, fullfill the promise.
+
+// Taken an array of links that are polled.
+
+//Add ability to check limits from headers later
+
+//Take the recipe and use it for a check and spawn the number of times remaining
+//When exhausted block for the given time.
 
 //var yo = R;
 //yo.bla();
@@ -18,34 +32,94 @@ var R = require("./tokenpoolrequest");
 //yo.checkLimitOnTokens();
 
 var options = {
-	url: 'https://api.github.com/search/repositories?q=Material+language:Java&sort=stars&order=desc',
+	url: 'https://api.github.com/search/repositories?q=Matdderial+language:Java&sort=stars&order=desc',
 	headers: {
 		'User-Agent': 'request'
 	}
 }
 
+// var TOKENCONFIG = {
+// 	tokenLocation : 'query', //or query 
+// 	tokenParam : 'access_token',  //etc
+// 	tokens : ['6f7c88588ebe3a853242ccd1f989e1814ada444b', 'alalalalall','8b17010f26db6cc54ce76932504bb3fe964dfa93'],
+// 	limitCheckPath : '/rate_limit',
+// 	baseUrl : 'https://api.github.com',
+// 	endpoints : ['search', 'resources'],
+// 	headerLimitRemainingVar : 'X-Ratelimit-Remaining',
+// 	headerLimitResetVar	: 'X-Ratelimit-Reset'
+// };
+
 var TOKENCONFIG = {
-	tokenLocation : 'query', //or query 
-	tokenParam : 'access_token',  //etc
-	tokens : ['6f7c88588ebe3a853242ccd1f989e1814ada444b', 'alalalalall','8b17010f26db6cc54ce76932504bb3fe964dfa93'],
-	limitCheckPath : '/rate_limit',
-	baseUrl : 'https://api.github.com',
-	endpoints : ['search', 'resources'],
-	headerLimitRemainingVar : 'X-Ratelimit-Remaining',
-	headerLimitResetVar	: 'X-Ratelimit-Reset'
+	tokenLocation : 'header', //or query 
+	tokenIdentifier : 'Token',  //etc
+	tokens : ['9a2b71d92c9053e913030f0b2bd92e83fcd439f5'],
+	// limitCheckPath : '/rate_limit',
+	// baseUrl : 'https://api.github.com',
+	// endpoints : ['search', 'core'],
+	//headerLimitRemainingVar : 'x-ratelimit-remaining',
+	failureResponseCode : 403, //Default to 403
+	headerLimitResetVar	: 'x-ratelimit-reset'
 };
 
 yo = new R(TOKENCONFIG);
 
-options.headers['Authorization'] = 'lora';
-
 //yo.setTokenLimitInfoRecipe(yo.gitHubLimitInfoRecipe);
 //yo.requestLimitInfoOnTokens();
-yo.generateResourcePool(TOKENCONFIG);
-yo.authenticatedRequest(options, 'WTC', 'search').then(function(data){
-	//console.log(error);
-	//console.log(data);
-}, function(err){
-	console.log(err);
-});
+//yo.generateResourcePool(TOKENCONFIG);
+
+ var promisesArray = [];
+ for(var i = 0; i < 35; i++){
+ 	promisesArray.push(yo.authenticatedRequest(options, 'WTC', 'search'));
+ }
+
+ Q.all(promisesArray).then(function(responses){
+ 	for(var i = 0; i < responses.length; i++){
+ 		console.log(responses[i].statusCode);
+ 	}
+ })
+
+// Q.call(yo.requestLimitInfoOnTokens()).then(
+// var promisesArray = [];
+// for(var i = 0; i < 35; i++){
+// 	promisesArray.push(yo.authenticatedRequest(options, 'WTC', 'search'));
+// }
+// return Q.all(promisesArray);
+
+// Q.all(promisesArray).then(function(){
+//  	console.log(yo.resourcePool);
+//}
+//)
+//).then();
+
+// var test = {
+// 	i : 100
+// }
+
+// function test1(callback){
+// 	callback(test.i);
+// 	test.i --;
+// }
+
+// for(var j = 0; j < 100; j++){
+// 	test1(console.log);
+// }
+
+// for(var i =0; i < 20; i++){
+//   Q.delay(i*1000).then(function explode() {
+//     //throw new Error("boo!");
+//     console.log("Yo");
+//   });
+// }
+
+// var setTimeout = (function(d){
+// 	//console.log(d);
+// 	return Q.defer().promise;
+// }, 1000, i);
+
+// yo.authenticatedRequest(options, 'WTC', 'search').then(function(data){
+// 	//console.log(error);
+// 	console.log(data.headers);
+// }, function(err){
+// 	console.log(err);
+// });
 //console.log((new Date).getTime());
--- tokenpoolrequest.js
+++ tokenpoolrequest.js
@@ -9,8 +9,8 @@ var TOKENPOOLCONFIG = {
 	'limitCheckPath' : '/rate_limit',
 	'baseUrl' : 'baseUrl',
 	'endpoints' : [],
-	'headerLimitRemainingVar' : 0,
-	'headerLimitResetVar'	: 0
+	//'headerLimitRemainingVar' : 0,
+	//'headerLimitResetVar'	: 0
 };
 
 var resource_pool = {
@@ -35,6 +35,16 @@ var TokenPoolRequest = function(tokenPoolConfig) {
 	var self = this;
 	self.Request = Request;
 	self.tokenPoolConfig = tokenPoolConfig;
+
+	if(tokenPoolConfig.tokens.length == 0){
+		throw new Error("No tokens found");
+	}
+
+	self.tokenPoolConfig['currToken'] = 0;
+
+	if(self.tokenPoolConfig['resetOffset'] == null){
+		self.tokenPoolConfig['resetOffset'] = 5; //Set a default to 5 sec
+	}
 	self.tokenLimitInfo = null;
 	self.tokenLimitInfoRecipe = null;
 	self.resourcePool = null;
@@ -73,72 +83,112 @@ TokenPoolRequest.prototype.getTokenPoolConfig = function(){
 	return self.tokenPoolConfig;
 };
 
+TokenPoolRequest.prototype.getTokenFromPool = function(){
+	var self = this;
+	var i =  self.tokenPoolConfig.currToken;
+	if(i == (self.tokenPoolConfig.tokens.length - 1)){
+		self.tokenPoolConfig.currToken = 0;
+	}else{
+		self.tokenPoolConfig.currToken++; 
+	}
+	return self.tokenPoolConfig.tokens[i];
+}
+
 //wait till completion 'WTC', fill or kill 'FOK'
-TokenPoolRequest.prototype.authenticatedRequest = function(options, type, resourceName){
+TokenPoolRequest.prototype.authenticatedRequest = function(options, type){
 	var deferred = Q.defer();
 	var self = this;
-	var tokenInfo = self.resourcePool[resourceName];
-	var tokenToApply = null;
+	//var tokenLimitInfo = self.tokenLimitInfo[resourceName];
+	//console.log(tokenLimitInfo);
+	var token = self.getTokenFromPool();
+	console.log(token);
 	var resets = [];
 
-	if(tokenInfo.length == 0){
-		deferred.reject(new Error("No tokens found"));
-	}
-
-	console.log("tokensFound");
-
-	for(var i in tokenInfo){
-		resets.push(tokenInfo[i].reset);
-		if(tokenInfo[i].remaining != 0 && tokenToApply == null){
-			tokenToApply = i;
-			tokenInfo[i].remaining--;
-			//break;
-		}
-	}
+	// for(var i in tokenInfo){
+	// 	resets.push(tokenInfo[i].reset);
+	// 	console.log("Tokens Remaining: " + tokenInfo[i].remaining);
+	// 	if(tokenLimitInfo[i].remaining != 0 && tokenLimitInfo == null){
+	// 		tokenToApply = i;
+	// 		tokenInfo[i].remaining--;
+	// 		//break;
+	// 	}
+	// }
 
 	// for(var i in tokenInfo){
 	// 	resets.push(tokenInfo[i].reset);
 	// }
 
-	console.log(resets);
+	//console.log(resets);
 
-	if(tokenToApply == 'null'){
-		if(type == 'WTC'){
-			var earliestReset = Math.min(resets);
-			if(earliestReset = -1){
-				console.log('No info found');
-				deferred.reject(new Error("No info on token reset found"));
-			}else{
-				var currEpoch = Math.floor((new Date).getTime()/1000); 
-				console.log("Going to sleep");
-				sleep(earliestReset - epochEpoch);
-				self.authenticatedRequest(options, type, resourceName);
-				return;
-			}
-		}
-	}
+	// if(tokenToApply == 'null'){
+	// 	console.log("no more tokens");
+	// 	if(type == 'WTC'){
+	// 		var earliestReset = Math.min(resets);
+	// 		if(earliestReset = -1){
+	// 			console.log('No info found');
+	// 			deferred.reject(new Error("No info on token reset found"));
+	// 		}else{
+	// 			var currEpoch = Math.floor((new Date).getTime()/1000); 
+	// 			console.log("Going to sleep");
+	// 			sleep(earliestReset - epochEpoch);
+	// 			self.authenticatedRequest(options, type, resourceName);
+	// 			return;
+	// 		}
+	// 	}
+	// }
 
-	console.log("Before Request");
-	self.applyToken(options, tokenToApply);
+	self.insertTokenInOptions(options, token);
 
-	Request(options, function (error, response, body) {
-    	if (error) {
-    		//console.log(error);
-        	deferred.reject(new Error(error));
-    	} else {
-    		try{
-    			tokenInfo[tokenToApply].reset = rawHeaders[self.tokenPoolConfig[headerLimitResetVar]];
-    		}catch(e){
-    			//do nothing 
-    		}
-        	deferred.resolve(response);
-    	}
-	});
+	self.makeRequest(options, deferred, type);
+
+	// Request(options, function (error, response, body) {
+ //    	if (error) {
+ //    		//console.log(error);
+ //        	deferred.reject(new Error(error));
+ //    	} else {
+ //    		try{
+ //    			var limitReset = response.headers[self.tokenPoolConfig['headerLimitResetVar']];
+ //    			var limitRemaining = response.headers[self.tokenPoolConfig['headerLimitRemainingVar']];
+ //    			console.log(limitReset);
+ //    			console.log(limitRemaining);
+ //    			self.resourcePool[resourceName].reset = limitReset;
+ //    		    self.resourcePool[resourceName].remaining = limitRemaining;
+ //    		}catch(e){
+ //    			//do nothing 
+ //    		}
+ //        	deferred.resolve(response);
+ //    	}
+	// });
 	return deferred.promise;
 }
 
+// Recallable wrapper function for the web request
+TokenPoolRequest.prototype.makeRequest = function(options, deferred, type){
+	var self = this;
+	Request(options, function (error, response, body){
+		if(error){
+			deferred.reject(new Error(error));
+		}else{
+     		var limitReset = response.headers[self.tokenPoolConfig['headerLimitResetVar']];
+			var currEpoch = Math.floor((new Date).getTime()/1000);
+			var waitPeriod = limitReset - currEpoch + self.tokenPoolConfig.resetOffset;
+			if(response.statusCode == self.tokenPoolConfig.failureResponseCode){
+				if(type == 'WTC'){
+					console.log("Blocked for " + waitPeriod + " seconds");
+					Q.delay(waitPeriod * 1000)
+					 .then(function(){
+					 	self.makeRequest(options, deferred, type)
+					 });
+				}
+			}else{
+				deferred.resolve(response);
+			}
+		}
+	});
+}
+
 //Beta: probably never used
-TokenPoolRequest.prototype.requestLimitInfoOnTokens = function(recipe, callback){
+TokenPoolRequest.prototype.requestLimitInfoOnTokens = function(callback){
 	var self = this;
 
 	if(self.tokenPoolConfig == null) 
@@ -151,8 +201,7 @@ TokenPoolRequest.prototype.requestLimitInfoOnTokens = function(recipe, callback)
 		throw new Error("Invalid baseUrl or checkLimitPath in given tokenPoolConfig");
 	}
 
-	for (var i in self.tokenPoolConfig.tokens){
-		
+	for (var i in self.tokenPoolConfig.tokens){	
 		var options = {
 			url: checkLimitUrl,
 			headers: {
@@ -166,6 +215,10 @@ TokenPoolRequest.prototype.requestLimitInfoOnTokens = function(recipe, callback)
 
 	Q.all(promisesArray).then(function(limitResponses){
 		self.tokenLimitInfo = self.tokenLimitInfoRecipe(limitResponses);
+
+		if(callback != null){
+			callback(self.tokenLimitInfo);	
+		}
 	});
 };
 
@@ -184,8 +237,15 @@ TokenPoolRequest.prototype.promisedRequest = function(options) {
 
 //Beta: probably never used
 TokenPoolRequest.prototype.gitHubLimitInfoRecipe = function(limitResponses){
+	console.log("Recipe Called");
 	var limits = {
 		search: {
+			total: 0,
+			induvidual: [
+
+			]
+		},
+		core: {
 			total: 0,
 			induvidual: [
 
@@ -194,33 +254,47 @@ TokenPoolRequest.prototype.gitHubLimitInfoRecipe = function(limitResponses){
 	};
 	for(var i in limitResponses){
 		var statusCode = limitResponses[i].statusCode;
+
 		if(statusCode == 200){
 			var body = JSON.parse(limitResponses[i].body);
-			//console.log(body.resources.search.remaining);
+
 			limits.search.total += body.resources.search.remaining;
-			var induvidual = {
-				'tokenId' : i,
-				'remaining' : body.resources.search.remaining,
-				'reset' : body.resources.search.reset
-			}
-			limits.search.induvidual.push(induvidual);
+			limits.core.total += body.resources.core.remaining;
+
+			var induvidualSearch = {
+				tokenId : i,
+				remaining : body.resources.search.remaining,
+				reset : body.resources.search.reset
+			};
+
+			var induvidualCore = {
+				tokenId : i,
+				remaining : body.resources.core.remaining,
+				reset : body.resources.core.reset
+			};
+
+			limits.search.induvidual.push(JSON.stringify(induvidualSearch));
+			console.log(limits);
+			limits.core.induvidual.push(JSON.stringify(induvidualCore));
+
+		}else{
+			return null;
 		}
 	}
-	console.log(limits);
 	return limits;
-	//console.log(limits);
 }
 
-//Beta: probably never used
-TokenPoolRequest.prototype.applyToken = function(options, tokenIndex){
+//Beta: pr
+TokenPoolRequest.prototype.insertTokenInOptions = function(options, token){
 	var self = this;	
 	if(self.tokenPoolConfig.tokenLocation == "header"){
-		options.headers['Authorization'] = self.tokenPoolConfig.tokenParam + " " + self.tokenPoolConfig.tokens[tokenIndex];
+		options.headers['Authorization'] = self.tokenPoolConfig.tokenIdentifier + " " + token;
+		console.log(options.headers['Authorization']);
 	}else if(self.tokenPoolConfig.tokenLocation == "query"){
 		if(options.qs == null){
 			options.qs = {};
 		}
-		options.qs[self.tokenPoolConfig.tokenParam] = self.tokenPoolConfig.tokens[tokenIndex];
+		options.qs[self.tokenPoolConfig.tokenIdentifier] = token;
 	}
 }
 
