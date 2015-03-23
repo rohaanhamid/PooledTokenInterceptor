41f7bd57d52aff5537b3b8c711ba0d2179b65016
Rohaan Hamid <rohaanhamid@hotmail.com>
2015-03-23 06:59:27 -0400

    First Beta Release

---
+++ index.js
@@ -0,0 +1,99 @@
+var Request = require('request');
+var Q = require('q');
+var ValidUrl = require('valid-url');
+
+var TokenPoolRequest = function(tokenPoolConfig) {
+	var self = this;
+	self.Request = Request;
+
+	if(tokenPoolConfig.tokens == null){
+		throw new Error("No tokens found");
+	}
+
+	self.tokenPoolConfig = tokenPoolConfig;
+
+	self.tokenPoolConfig['currToken'] = 0; //Start by the first token
+
+	if(self.tokenPoolConfig['resetOffset'] == null){
+		self.tokenPoolConfig['resetOffset'] = 5; //Set a default to 5 sec
+	}
+};
+
+TokenPoolRequest.prototype.setTokenPoolConfig = function(tokenPoolConfig){
+	var self = this;
+	self.tokenPoolConfig = tokenPoolConfig;
+};
+
+TokenPoolRequest.prototype.getTokenPoolConfig = function(){
+	var self = this;
+	return self.tokenPoolConfig;
+};
+
+// Distribute tokens from the supplied pool in a round robin pattern
+TokenPoolRequest.prototype.getTokenFromPool = function(){
+	var self = this;
+	var i =  self.tokenPoolConfig.currToken;
+	if(i == (self.tokenPoolConfig.tokens.length - 1)){
+		self.tokenPoolConfig.currToken = 0;
+	}else{
+		self.tokenPoolConfig.currToken++; 
+	}
+	return self.tokenPoolConfig.tokens[i];
+}
+
+TokenPoolRequest.prototype.authenticatedRequest = function(options, type){
+	var deferred = Q.defer();
+	var self = this;
+
+	var token = self.getTokenFromPool();
+	var resets = [];
+
+	self.insertTokenInOptions(options, token);
+
+	self.makeRequest(options, deferred, type);
+
+	return deferred.promise;
+}
+
+// Recallable wrapper function for the web request
+TokenPoolRequest.prototype.makeRequest = function(options, deferred, type){
+	var self = this;
+	Request(options, function (error, response, body){
+		if(error){
+			deferred.reject(new Error(error));
+		}else{
+     		var limitReset = response.headers[self.tokenPoolConfig['headerLimitResetVar']];
+			var currEpoch = Math.floor((new Date).getTime()/1000);
+			var waitPeriod = limitReset - currEpoch + self.tokenPoolConfig.resetOffset;
+			if(response.statusCode == self.tokenPoolConfig.failureResponseCode){
+				// Wait till all the requests are successful or just return whatever
+				// comes back
+				if(type == 'WTC'){
+					console.log("Blocked for " + waitPeriod + " seconds");
+					Q.delay(waitPeriod * 1000)
+					 .then(function(){
+					 	self.makeRequest(options, deferred, type)
+					 });
+				}else{
+					deferred.resolve(response);
+				}
+			}else{
+				deferred.resolve(response);
+			}
+		}
+	});
+}
+
+TokenPoolRequest.prototype.insertTokenInOptions = function(options, token){
+	var self = this;	
+	if(self.tokenPoolConfig.tokenLocation == "header"){
+		options.headers['Authorization'] = self.tokenPoolConfig.tokenIdentifier + " " + token;
+	}else if(self.tokenPoolConfig.tokenLocation == "query"){
+		if(options.qs == null){
+			options.qs = {};
+		}
+		options.qs[self.tokenPoolConfig.tokenIdentifier] = token;
+	}
+}
+
+module.exports = TokenPoolRequest;
--- test.js
+++ test.js
@@ -1,125 +1,34 @@
-var R = require("./tokenpoolrequest"); 
+var PooledTokenInterceptor = require("./index"); 
 var Q = require("q");
 
-//1. Check limits on all token pools using given recipe
-//2. When token pool is exhausted, request new resets and wait for the earliest reset
-// if 'WTC' is selected. Else return all promise with an error of pool exhausted.
-//3. On wakeup, recursively call the function and make sure current request is not
-// executed. Once the request is successful, fullfill the promise.
-
-// Taken an array of links that are polled.
-
-//Add ability to check limits from headers later
-
-//Take the recipe and use it for a check and spawn the number of times remaining
-//When exhausted block for the given time.
-
-//var yo = R;
-//yo.bla();
-//yo.set("lun kabab");
-//yo.print();
-//yo.printA();
-//yo.printShit();
-//yo.Request.printLora();
-
-// var r = new R();
-// yo.Request('http://www.google.com', function (error, response, body) {
-//   if (!error && response.statusCode == 200) {
-//     console.log(body) // Show the HTML for the Google homepage. 
-//   }
-// });
-
-//yo.checkLimitOnTokens();
-
 var options = {
-	url: 'https://api.github.com/search/repositories?q=Matdderial+language:Java&sort=stars&order=desc',
+	url: 'https://api.github.com/search/repositories?q=Material+language:Java&sort=stars&order=desc',
 	headers: {
 		'User-Agent': 'request'
 	}
 }
 
-// var TOKENCONFIG = {
-// 	tokenLocation : 'query', //or query 
-// 	tokenParam : 'access_token',  //etc
-// 	tokens : ['6f7c88588ebe3a853242ccd1f989e1814ada444b', 'alalalalall','8b17010f26db6cc54ce76932504bb3fe964dfa93'],
-// 	limitCheckPath : '/rate_limit',
-// 	baseUrl : 'https://api.github.com',
-// 	endpoints : ['search', 'resources'],
-// 	headerLimitRemainingVar : 'X-Ratelimit-Remaining',
-// 	headerLimitResetVar	: 'X-Ratelimit-Reset'
-// };
-
-var TOKENCONFIG = {
-	tokenLocation : 'header', //or query 
-	tokenIdentifier : 'Token',  //etc
-	tokens : ['9a2b71d92c9053e913030f0b2bd92e83fcd439f5'],
-	// limitCheckPath : '/rate_limit',
-	// baseUrl : 'https://api.github.com',
-	// endpoints : ['search', 'core'],
-	//headerLimitRemainingVar : 'x-ratelimit-remaining',
-	failureResponseCode : 403, //Default to 403
-	headerLimitResetVar	: 'x-ratelimit-reset'
+var tokenConfig = {
+	tokenLocation : 'header', //or 'query' 
+	tokenIdentifier : 'Token', // or whatever the API requires.
+	tokens : [INSERT TOKENS HERE],
+	failureResponseCode : 403, // Default to 403. This is used by the module to check if the request failed. Check what the API being used with send back.
+	headerLimitResetVar	: 'x-ratelimit-reset' // Paramter name in the response header that holds then time when the limit will be reset
 };
 
-yo = new R(TOKENCONFIG);
+var request = new PooledTokenInterceptor(tokenConfig);
 
-//yo.setTokenLimitInfoRecipe(yo.gitHubLimitInfoRecipe);
-//yo.requestLimitInfoOnTokens();
-//yo.generateResourcePool(TOKENCONFIG);
+var promisesArray = [];
 
- var promisesArray = [];
- for(var i = 0; i < 35; i++){
- 	promisesArray.push(yo.authenticatedRequest(options, 'WTC', 'search'));
- }
+// Collect promised for each of the requests to the API
+for(var i = 0; i < 45; i++){
+	promisesArray.push(request.authenticatedRequest(options, ''));
+}
 
- Q.all(promisesArray).then(function(responses){
- 	for(var i = 0; i < responses.length; i++){
- 		console.log(responses[i].statusCode);
- 	}
- })
-
-// Q.call(yo.requestLimitInfoOnTokens()).then(
-// var promisesArray = [];
-// for(var i = 0; i < 35; i++){
-// 	promisesArray.push(yo.authenticatedRequest(options, 'WTC', 'search'));
-// }
-// return Q.all(promisesArray);
-
-// Q.all(promisesArray).then(function(){
-//  	console.log(yo.resourcePool);
-//}
-//)
-//).then();
-
-// var test = {
-// 	i : 100
-// }
-
-// function test1(callback){
-// 	callback(test.i);
-// 	test.i --;
-// }
-
-// for(var j = 0; j < 100; j++){
-// 	test1(console.log);
-// }
-
-// for(var i =0; i < 20; i++){
-//   Q.delay(i*1000).then(function explode() {
-//     //throw new Error("boo!");
-//     console.log("Yo");
-//   });
-// }
-
-// var setTimeout = (function(d){
-// 	//console.log(d);
-// 	return Q.defer().promise;
-// }, 1000, i);
-
-// yo.authenticatedRequest(options, 'WTC', 'search').then(function(data){
-// 	//console.log(error);
-// 	console.log(data.headers);
-// }, function(err){
-// 	console.log(err);
-// });
-//console.log((new Date).getTime());
+// Wait for the promised to resolve and print out the statusCode for each of them
+Q.all(promisesArray).then(function(responses){
+	console.log("Number of responses: " + responses.length);
+	for(var i = 0; i < responses.length; i++){
+		console.log(responses[i].statusCode);
+	}
+})
